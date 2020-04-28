a13a80ae23972e082465ddb17bf651e86caa7116
wangtao <tao.wang@smartriver.com.cn>
2020-04-28 19:26:34 +0800

    修改可以自定义模板路径

--- src/main/java/com/power/doc/utils/BeetlTemplateUtil.java
+++ src/main/java/com/power/doc/utils/BeetlTemplateUtil.java
@@ -51,17 +51,17 @@ public class BeetlTemplateUtil {
      * @param templateName template name
      * @return Beetl Template Object
      */
-//    public static Template getByName(String templateName) {
-//        try {
-//            ClasspathResourceLoader resourceLoader = new ClasspathResourceLoader("/template/");
-//            Configuration cfg = Configuration.defaultConfiguration();
-//            GroupTemplate gt = new GroupTemplate(resourceLoader, cfg);
-//            System.out.println(resourceLoader.getResource(""));
-//            return gt.getTemplate(templateName);
-//        } catch (IOException e) {
-//            throw new RuntimeException("Can't get Beetl template.");
-//        }
-//    }
+    public static Template getByName(String templateName) {
+        try {
+            ClasspathResourceLoader resourceLoader = new ClasspathResourceLoader("/template/");
+            Configuration cfg = Configuration.defaultConfiguration();
+            GroupTemplate gt = new GroupTemplate(resourceLoader, cfg);
+            System.out.println(resourceLoader.getResource(""));
+            return gt.getTemplate(templateName);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't get Beetl template.");
+        }
+    }
 
     /**
      * 通过外部指定的目录地址获取 beetl模板对象
@@ -78,17 +78,22 @@ public class BeetlTemplateUtil {
             if (StringUtils.isBlank(templateDir)) {
                 templateDir = "/template/";
             }
-            //将windows中的双反斜杠 修改成 正斜杠
-            templateDir = templateDir.replace("\\", "/").trim();
-            if (templateDir.charAt(templateDir.length() - 1) != '/') {
-                //不是以斜杠结尾 补全斜杠
-                templateDir += "/";
-            }
-
-            ResourceLoader resourceLoader = new FileResourceLoader(templateDir);
+            ClasspathResourceLoader resourceLoader = new ClasspathResourceLoader(templateDir);
             Configuration cfg = Configuration.defaultConfiguration();
             GroupTemplate gt = new GroupTemplate(resourceLoader, cfg);
+            System.out.println(resourceLoader.getResource(""));
             return gt.getTemplate(templateName);
+            //将windows中的双反斜杠 修改成 正斜杠
+//            templateDir = templateDir.replace("\\", "/").trim();
+//            if (templateDir.charAt(templateDir.length() - 1) != '/') {
+//                //不是以斜杠结尾 补全斜杠
+//                templateDir += "/";
+//            }
+//
+//            ResourceLoader resourceLoader = new FileResourceLoader(templateDir);
+//            Configuration cfg = Configuration.defaultConfiguration();
+//            GroupTemplate gt = new GroupTemplate(resourceLoader, cfg);
+//            return gt.getTemplate(templateName);
         } catch (IOException e) {
             throw new RuntimeException("Can't get Beetl template.");
         }
